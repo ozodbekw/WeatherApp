8f4903b7aa61a5899003f2f2cc8464dcfc37363d
Ozodbek Tursunaliyev <ozodbektursunaliyev11@gmail.com>
2025-01-27 22:08:35 +0500

    Add initial implementation of Ozodbek Weather application with HTML, CSS, and JavaScript

---
+++ js/forecast.js
@@ -0,0 +1,12 @@
+// request data
+const getData = async (city) => {
+  const base = "https://api.openweathermap.org/data/2.5/weather";
+  const query = `?q=${city}&units=metric&appid=${env.API__KEY}`;
+  loader(true);
+  const request = await fetch(base + query);
+  const data = await request.json();
+  loader(false);
+  return data;
+};
+
+const result = getData("London").then((data) => console.log(data));
