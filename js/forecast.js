0be810c107615020a330a87fdb318be79a457d3a
Ozodbek Tursunaliyev <ozodbektursunaliyev11@gmail.com>
2025-01-27 22:15:57 +0500

    Refactor API key handling in forecast.js to use a constant variable

--- js/forecast.js
+++ js/forecast.js
@@ -1,7 +1,9 @@
+const API__KEY = "a75262e3120c70b5840c6f8c59fd8e5a";
+
 // request data
 const getData = async (city) => {
   const base = "https://api.openweathermap.org/data/2.5/weather";
-  const query = `?q=${city}&units=metric&appid=${meta.env.API__KEY}`;
+  const query = `?q=${city}&units=metric&appid=${API__KEY}`;
   loader(true);
   const request = await fetch(base + query);
   const data = await request.json();
