5609905b33f891de4015753da15128092c61c13c
Ozodbek Tursunaliyev <ozodbektursunaliyev11@gmail.com>
2025-01-27 22:12:26 +0500

    Fix API key reference in forecast.js for weather data fetching

--- js/forecast.js
+++ js/forecast.js
@@ -1,7 +1,7 @@
 // request data
 const getData = async (city) => {
   const base = "https://api.openweathermap.org/data/2.5/weather";
-  const query = `?q=${city}&units=metric&appid=${env.API__KEY}`;
+  const query = `?q=${city}&units=metric&appid=${API__KEY}`;
   loader(true);
   const request = await fetch(base + query);
   const data = await request.json();
